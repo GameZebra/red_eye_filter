a28df7b88d5037a24f41c66a7e9becfdc617650d
GameZebra <mr.xmitkotox@gmail.com>
2022-09-02 22:34:00 +0300

    finds red eyes and determinets which type they are

--- python/solution.py
+++ python/solution.py
@@ -5,7 +5,13 @@ from typing import (
     Tuple,
     Union
 )
-from utils.eye_pattern import EYE_PATTERN_1
+
+from utils.eye_pattern import (
+    EYE_PATTERN_1, 
+    EYE_PATTERN_2, 
+    EYE_PATTERN_3, 
+    EYE_PATTERN_4
+)
 
 from utils.image import (
     ImageType,
@@ -15,25 +21,39 @@ from utils.image import (
 
 from utils.function_tracer import FunctionTracer
 
-import utils.eye_pattern 
+
 
 def is_red_eye(image: Union[PackedImage, StrideImage], x: int, y: int, checked_pixels: List):
-    result = True
-    for i in range(5):
-        for j in range(5):
-            if i == 0 and j == 0:
-                    continue    
-        is_red = image.pixels[image.resolution.height*(y+i)+(x+j)].red >= 200
-        is_non_white = EYE_PATTERN_1[i][j] != ' '
-    
+    status = [1, 1, 1, 1]
+    patterns = [EYE_PATTERN_1, EYE_PATTERN_2, EYE_PATTERN_3, EYE_PATTERN_4]
+    for index, pattern in enumerate(patterns):   
+        break_flag = False
+        for i in range(5):
+            for j in range(5):
+                if i == 0 and j == 0:
+                        continue
+                is_red = image.pixels[image.resolution.height*(y+i)+(x+j)].red >= 200
+                is_white = pattern[i][j] == ' '
+                if is_white:
+                    continue
+                if not is_red:
+                    status[index] = 0
+                    break_flag = True
+            if break_flag:
+                break
+
     #if true add them to checked_pixels 
-    if result == True:
+    print(status)
+    if 1 in status:
+        result = True
         for i in range(5):
             for j in range(5):
                 if i == 0 and j == 0:
                     continue
                 checked_pixels.append(image.resolution.height*(y+i)+(x+j))
-    return result
+    else:
+        result = False
+    return [result, status]
 
 
 def compute_solution(images: List[Union[PackedImage, StrideImage]]):
@@ -48,14 +68,15 @@ def compute_solution(images: List[Union[PackedImage, StrideImage]]):
                 else:
                     checked_pixels.append(image.resolution.height*y+x)
                 if image.pixels[image.resolution.height*y+x].red >= 200:
-                    if image.resolution.width - x < 5 or image.resolution.height - y < 5:
+                    if image.resolution.width - x-1 <= 5 or image.resolution.height - y-1 <= 5:
                         continue
                     #start checking
+                    is_red, status = is_red_eye(image, x, y, checked_pixels)
                     #if checking true: correct
+                    if is_red:
+                        
                     #continue
                     #if checking false: add coordinates as checked
-                    pass
-        is_red_eye(image, x, y, checked_pixels)
         break
 
     
